afa25b86e75dfd4234bf2d29716309dd8fe43862
sudoshreyansh <shreyanshjain14072002@gmail.com>
2024-01-16 02:15:58 +0530

    update env variables

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,13 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomiclabs/hardhat-ethers";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {
       forking: {
-        url: 'https://eth-mainnet.g.alchemy.com/v2/0SloeqPBVEv90hh23uyA_iRe0wtQE7Kc',
+        url: process.env.ALCHEMY_URI!,
         blockNumber: 19013000
       }
     }
