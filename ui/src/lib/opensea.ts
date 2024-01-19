0bf7381d698d2ec82084580e9e4478cce2dd8221
sudoshreyansh <shreyanshjain14072002@gmail.com>
2024-01-19 08:43:57 +0530

    add opensea hooks

---
+++ ui/src/lib/opensea.ts
@@ -0,0 +1,38 @@
+import useSWR from 'swr';
+
+const fetcher = (uri: string) => fetch(uri, {
+  headers: {
+    'x-api-key': process.env.NEXT_PUBLIC_OPENSEA_KEY!,
+    'Accept': 'application/json'
+  }
+}).then(res => res.json())
+
+export type Token = {
+  name: string;
+  description: string;
+  identifier: string;
+  contract: string;
+  collection: string;
+  opensea_url: string;
+  image_url: string;
+}
+
+export const useTokenData = (address: string, id: string, chain: string) => {
+  const { data, error, isLoading } = useSWR(`https://api.opensea.io/api/v2/chain/${chain}/contract/${address}/nfts/${id}`, fetcher)
+ 
+  return {
+    data: data as Token,
+    isLoading,
+    isError: error
+  }
+}
+
+export const useAccountTokensData = (address: string, chain: string) => {
+  const { data, error, isLoading } = useSWR(`https://api.opensea.io/api/v2/chain/${chain}/account/${address}/nfts`, fetcher)
+ 
+  return {
+    data: data.nfts as Token[],
+    isLoading,
+    isError: error
+  }
+}
